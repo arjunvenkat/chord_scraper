1b1e2a999a98bf17a348559c8e02a6a254abc5e9
Arjun Venkataswamy <avenkat2@gmail.com>
2012-09-12 14:47:13 -0500

    added loop and mechanize functionality to scrape multiple pages automatically

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,23 +23,53 @@ require 'open-uri'
     #@chords = @chords.scan(/\w+/)
     
     page_url = params['page_url']
+
+    @songs = []
     
-    @chords = []
+    @chord_page_links = []
     agent = Mechanize.new
-    agent.get(page_url) do |page|
-      table = page.search('table[cellspacing="0"][cellpadding="2"]')
+    @page = agent.get(page_url)
+
+    until @page == nil
+      puts "loop"
+      table = @page.search('table[cellspacing="0"][cellpadding="2"]')
       table.css('tr').each do |row|
         next if row.css('td:nth-child(3)').blank?
         
         if row.css('td:nth-child(3)').text == "Chords"
           #row.css('td:nth-child(1) > a').each{|link| @chords << link['href']}
-          @chords << row.css('td:nth-child(1) > a').attr('href').text
+          @chord_page_links << row.css('td:nth-child(1) > a').attr('href').text
         end
       end
       
+      if @page.link_with(:text => /^Next/) != nil
+        @page = @page.link_with(:text => /^Next/).click
+      else
+        @page = nil
+      end
 
-    end 
+    end  
+  
+
     #@chords = @chords.split(/\s\S\s\s/)
+    @chord_page_links.each do |song_link|
+      test_page = agent.get(song_link)
+      title = test_page.search('td.fs-10 h1').text
+      chords = []
+      pre_div = test_page.search('pre').css('span').each do |chord|
+        chords << chord.text
+      end 
+      chords.uniq!
+
+      @songs << [title, chords]
+    end
+
+    
+    
+
+
+    #   @chords << node.text
+    # end
 
   end
 
